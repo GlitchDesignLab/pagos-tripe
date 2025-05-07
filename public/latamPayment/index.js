92525342f56a65694f94e27729085527a07ef483
Marcos <lavarellomarcos@gmail.com>
2025-05-07 09:34:03 -0300

    mp port changed to 3001

--- public/latamPayment/index.js
+++ public/latamPayment/index.js
@@ -55,7 +55,7 @@ app.post("/crear-preferencia", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
